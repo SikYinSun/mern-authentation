2997713cef6bece2be0b76857d54ad1a88ab1a7f
samuels <sysun@ucsd.edu>
2023-12-04 17:08:38 -0500

    update file name

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 
 export default function App() {
   return (
-    <div className='text-red-400'>
-      App
-    </div>
+
   )
 }
